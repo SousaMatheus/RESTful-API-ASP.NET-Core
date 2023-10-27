f7cef48ececf8a05513132e1d0dbe067199c39bb
Matheus Sousa <ma.sousa.soares@gmail.com>
2023-10-26 23:37:25 -0300

    Correção ViewModel Endereco e configuração de Injeção de dependência

--- src/API.MSDev/DevMs.Api/Configuration/DependecyInjectionConfig.cs
+++ src/API.MSDev/DevMs.Api/Configuration/DependecyInjectionConfig.cs
@@ -12,9 +12,14 @@ namespace DevMs.Api.Configuration
         {
             services.AddScoped<MeuDbContext> ();
             services.AddScoped<IFornecedorRepository, FornecedorRepository> ();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository> ();
+            services.AddScoped<IProdutoRepository, ProdutoRepository> ();
+
             services.AddScoped<IFornecedorService, FornecedorService> ();
+            services.AddScoped<IProdutoService, ProdutoService> ();
             services.AddScoped<IEnderecoRepository, EnderecoRepository> ();
             services.AddScoped<INotificador, Notificador> ();
+
             return services;
         }
     }
--- src/API.MSDev/DevMs.Api/ViewModels/EnderecoViewModel.cs
+++ src/API.MSDev/DevMs.Api/ViewModels/EnderecoViewModel.cs
@@ -22,6 +22,10 @@ namespace DevMs.Api.ViewModels
         public string Bairro{ get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(8, ErrorMessage = "O campo {0} precisa ter {2} caracteres", MinimumLength = 8)]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Cidade { get; set; }
 
